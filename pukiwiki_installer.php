98c3c3e804734d2995160af6055260d830a9be9a
Pitan MAD <58260965+PTOM76@users.noreply.github.com>
2022-03-07 16:14:01 +0900

    Add files via upload

--- pukiwiki_installer.php
+++ pukiwiki_installer.php
@@ -45,9 +45,9 @@
         if (file_exists("./" . basename($url))) {
             $zip = new ZipArchive();
             if ($zip->open("./" . basename($url)) === TRUE) {
+                $pkwkdirName = $zip->getNameIndex(0);
                 $zip->extractTo('./');
                 $zip->close();
-                $pkwkdirName = $zip->getNameIndex(0);
                 if(!dir_copy($pkwkdirName, __DIR__)){echo 'エラー: ディレクトリのコピーに失敗しました。もう一度試してみてください。';exit;}
                 if(!remove_dir($pkwkdirName)){echo "エラー: 解凍後のディレクトリが消えていない可能性があります。手動で削除してください。";}
                 if($_POST['del_tempzip'] = 'on'){
